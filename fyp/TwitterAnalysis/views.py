6a7057cd9e010124e2d3ed3387163466be621e5e
ML-Ubuntu <dannyoleary1@gmail.com>
2017-11-14 14:28:24 +0000

    Restructure of project

--- fyp/TwitterAnalysis/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("This is where the index page will go.")
---
+++ fyp/fyp_webapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FypWebappConfig(AppConfig):
+    name = 'fyp_webapp'
