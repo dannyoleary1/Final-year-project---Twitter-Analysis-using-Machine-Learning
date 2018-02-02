ba47ba7269388a929d8f9579ad10427990dc432b
danny <dannyoleary1@gmail.com>
2018-02-02 14:41:49 +0000

    removed un-wanted files

--- fyp/test.py
+++ fyp/test.py
@@ -4,9 +4,12 @@ from fyp_webapp.TwitterProcessing import preprocessor
 from fyp_webapp.TwitterProcessing import termsfrequency
 from fyp_webapp.TwitterProcessing import collect_old_tweets
 
+res = elastic_utils.iterate_search("psn")
+for entry in res:
+    print ("The number of tweets for the 19th hour was: %s, and this was on: %s" % (str(entry["_source"]["hour_breakdown"]["19"]), str(entry["_source"]["date"])))
 
 
-print(collect_old_tweets.run(query_search="psn", start_date="2018-01-24", end_date="2018-01-25"))
+#print(collect_old_tweets.run(query_search="psn", start_date="2018-01-24", end_date="2018-01-25"))
 
 
 
