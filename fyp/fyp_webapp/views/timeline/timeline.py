7dd7e3d265d387121546d4df4b8862c9ba0317c5
FYP Ubuntu Machine <dannyoleary1@gmail.com>
2018-04-17 20:31:43 +0100

    Slight fix for table

--- fyp/fyp_webapp/views/timeline/timeline.py
+++ fyp/fyp_webapp/views/timeline/timeline.py
@@ -13,18 +13,18 @@ def timeline(request):
         name =""
         for mod in cat:
             res = elastic_utils.iterate_search(index_name=mod.category_name, query={
+                "size": 20,
                         "query":
                             {
                                 "match_all": {}
                             },
                         "sort": [
                             {
-                                "last_time": {
+                                "date": {
                                     "order": "desc"
                                 }
                             }
                         ],
-                        "size":20,
                     })
             med = elastic_utils.search_index(index_name=mod.category_name+"-median")
             name = mod.category_name
@@ -34,18 +34,18 @@ def timeline(request):
         name = ""
         for mod in cat:
             res = elastic_utils.iterate_search(index_name=mod.category_name, query={
+                "size": 20,
                         "query":
                             {
                                 "match_all": {}
                             },
                         "sort": [
                             {
-                                "last_time": {
+                                "date": {
                                     "order": "desc"
                                 }
                             }
                         ],
-                        "size":20,
                     })
             med = elastic_utils.search_index(index_name=mod.category_name+"-median")
             name = mod.category_name
@@ -53,18 +53,22 @@ def timeline(request):
 
     cat = TwitterCat.objects.filter(user=request.user)
     data = {}
+    i = 0
     for entry in res:
         temp_data = {}
         for hour in entry["_source"]["hour_breakdown"]:
             temp_data[int(hour)] = (entry["_source"]["hour_breakdown"][hour])
         data[entry["_source"]["date"]] = temp_data
+        i+=1
+        if i == 20:
+            break
 
     day_median = med["hits"]["hits"][0]["_source"]["day_median"]
     hour_median = med["hits"]["hits"][0]["_source"]["hour_median"]
     minute_median = med["hits"]["hits"][0]["_source"]["minute_median"]
-    hour_med_tresh = hour_median*2
-    minute_med_tresh = minute_median*2
-    day_med_tresh = day_median*1.5
+    hour_med_tresh = round(hour_median*2, 2)
+    minute_med_tresh = round(minute_median*2, 2)
+    day_med_tresh = round(day_median*1.5,2)
 
     print (hour_med_tresh)
     #for entry in res:
