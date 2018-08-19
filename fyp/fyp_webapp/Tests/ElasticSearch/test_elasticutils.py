f83890c3b87d3b9d9cdd9db90a9e01dc187b5105
danny o'leary <dannyoleary1@gmail.com>
2018-08-19 21:54:25 +0100

    git added to root instead of internal, tests fixed, and support for travis ci.

--- fyp/fyp_webapp/Tests/ElasticSearch/test_elasticutils.py
+++ fyp/fyp_webapp/Tests/ElasticSearch/test_elasticutils.py
@@ -7,12 +7,12 @@ class ElasticUtilsTest(TestCase):
         es.create_index("test")
 
     def tearDown(self):
+        time.sleep(1)
         es.delete_index("test")
 
     def test_create_index(self):
-        es.delete_index("testcase")
-        time.sleep(1)
         self.assertIn(" \'index\': \'testcase\'", es.create_index("testcase")) #An index is made
+        time.sleep(1)
         es.delete_index("testcase")
 
     def test_delete_index(self):
@@ -25,20 +25,25 @@ class ElasticUtilsTest(TestCase):
     def test_list_all_index(self):
         es.create_index("list_all")
         res = es.list_all_indexes()
-        self.assertIn("\'test\'" , res)
-        self.assertIn("\'list_all\'", res)
+        time.sleep(1)
+        self.assertIn('test' , res)
+        self.assertIn('list_all', res)
         es.delete_index("list_all")
         es.delete_index("test")
-        self.assertIn("{}", res)
+        time.sleep(1)
+
 
     def test_add_entry(self):
         doc = {"name": "test"}
         res = es.add_entry(index_name="test", id=1, body=doc)
-        self.assertEqual(True, res['created'])
+        time.sleep(1)
+        self.assertEqual('created', res['result'])
         res = es.add_entry(index_name="test", id=2, body=doc) #This should also work
-        self.assertEqual(True, res['created'])
+        time.sleep(1)
+        self.assertEqual('created', res['result'])
         res = es.add_entry(index_name="test", id=1, body=doc) #This should fail since its the same id
-        self.assertEqual(False, res['created'])
+        time.sleep(1)
+        self.assertEqual('updated', res['result'])
 
 
     def test_delete_entry(self):
